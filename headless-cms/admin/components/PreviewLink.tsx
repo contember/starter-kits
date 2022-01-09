cd2f2fcf676bf103dffb0863466be42af95bee12
jonasNobile <jonas.nobile@icloud.com>
2022-01-09 12:52:29 +0100

    Rename component

--- headless-cms/admin/components/PreviewLink.tsx
+++ headless-cms/admin/components/PreviewLink.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { Component, Field, useEnvironment, useField } from '@contember/admin'
 
-type PreviewButtonProps = {
+type PreviewLinkProps = {
 	slugField: string,
 	prefix?: string,
 	path?: string
 }
 
-export const PreviewLink = Component<PreviewButtonProps>(
+export const PreviewLink = Component<PreviewLinkProps>(
 	({ slugField, path, prefix }) => {
 		const webUrl = useEnvironment().getValue('WEB_URL')
 		const { value: slug } = useField<string>(slugField)
@@ -27,5 +27,5 @@ export const PreviewLink = Component<PreviewButtonProps>(
 	({ slugField }) => (
 		<Field field={slugField} />
 	),
-	'PreviewButton'
+	'PreviewLink'
 )
