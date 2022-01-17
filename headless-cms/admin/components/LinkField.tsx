814f9ee4a0cb0229987befb3e3b6f0e761b16704
jonasNobile <jonas.nobile@icloud.com>
2022-01-17 16:21:38 +0100

    Headless CMS: format

--- headless-cms/admin/components/LinkField.tsx
+++ headless-cms/admin/components/LinkField.tsx
@@ -8,19 +8,21 @@ export interface LinkFieldProps {
 
 export const LinkField = Component<LinkFieldProps>(
 	({ field, label }) => {
-		return <HasOne field={field}>
-			<DiscriminatedBlocks label={label ?? 'Link'} field="type" description="Select what you want to link.">
-				<Block discriminateBy="article" label="Articles">
-					<SelectField label={undefined} options="Article.slug" field="article" />
-				</Block>
-				<Block discriminateBy="page" label="Pages">
-					<SelectField label={undefined} options="Page.slug" field="page" />
-				</Block>
-				<Block discriminateBy="url" label="External link">
-					<TextField label="Url" field="url" />
-				</Block>
-			</DiscriminatedBlocks>
-		</HasOne>
+		return (
+			<HasOne field={field}>
+				<DiscriminatedBlocks label={label ?? 'Link'} field="type" description="Select what you want to link.">
+					<Block discriminateBy="article" label="Articles">
+						<SelectField label={undefined} options="Article.slug" field="article" />
+					</Block>
+					<Block discriminateBy="page" label="Pages">
+						<SelectField label={undefined} options="Page.slug" field="page" />
+					</Block>
+					<Block discriminateBy="url" label="External link">
+						<TextField label="Url" field="url" />
+					</Block>
+				</DiscriminatedBlocks>
+			</HasOne>
+		)
 	},
 	'LinkField',
 )
