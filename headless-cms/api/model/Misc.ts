3f98c4390ba90f59ec830ec6e81166d3162c648a
jonasNobile <jonas.nobile@icloud.com>
2022-01-17 16:22:38 +0100

    Headless CMS: update enums

--- headless-cms/api/model/Misc.ts
+++
@@ -1,3 +0,0 @@
-import { SchemaDefinition as def } from '@contember/schema-definition'
-
-export const One = def.createEnum('One')
--- headless-cms/api/model/Setting.ts
+++ headless-cms/api/model/Setting.ts
@@ -1,8 +1,8 @@
 import { SchemaDefinition as def } from '@contember/schema-definition'
-import { One, Content, Image } from '.'
+import { Content, Image } from '.'
 
 export class Setting {
-	unique = def.enumColumn(One).notNull().unique()
+	unique = def.enumColumn(def.createEnum('One')).notNull().unique()
 	logo = def.manyHasOne(Image)
 	footerCopyright = def.oneHasOne(Content).cascadeOnDelete()
 }
