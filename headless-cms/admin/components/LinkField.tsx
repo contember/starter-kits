3bf56493d41b7af7d7590e06af43f286c65bfc0f
jonasNobile <jonas.nobile@icloud.com>
2022-01-18 14:22:40 +0100

    Headless CMS: fix page without slug

--- headless-cms/admin/components/LinkField.tsx
+++ headless-cms/admin/components/LinkField.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react'
-import { Block, Component, DiscriminatedBlocks, HasOne, SelectField, TextField } from "@contember/admin"
+import { Block, Component, DiscriminatedBlocks, EntityAccessor, Field, HasOne, SelectField, TextField } from "@contember/admin"
 
 export interface LinkFieldProps {
 	field: string
 	label?: string
+}
+
+function customRenderOptions(accessor: EntityAccessor) {
+	const slug = accessor.getField("slug").value
+	const role = accessor.getField("role").value
+
+	return slug ?? role
 }
 
 export const LinkField = Component<LinkFieldProps>(
@@ -12,10 +19,25 @@ export const LinkField = Component<LinkFieldProps>(
 			<HasOne field={field}>
 				<DiscriminatedBlocks label={label ?? 'Link'} field="type" description="Select what you want to link.">
 					<Block discriminateBy="article" label="Articles">
-						<SelectField label={undefined} options="Article.slug" field="article" />
+						<SelectField 
+							label={undefined}
+							options="Article.slug"
+							field="article"
+						/>
 					</Block>
 					<Block discriminateBy="page" label="Pages">
-						<SelectField label={undefined} options="Page.slug" field="page" />
+						<SelectField
+							label={undefined}
+							options="Page"
+							field="page"
+							renderOption={customRenderOptions}
+							optionsStaticRender={
+								<>
+									<Field field="slug" />
+									<Field field="role" />
+								</>
+							}
+						/>
 					</Block>
 					<Block discriminateBy="url" label="External link">
 						<TextField label="Url" field="url" />
