3e97105a370d3308733d292c5ed7cf017d22a4d1
jonasNobile <jonas.nobile@icloud.com>
2022-02-08 13:55:56 +0100

    Headless CMS: add ISR

--- headless-cms/head/pages/blog/index.tsx
+++ headless-cms/head/pages/blog/index.tsx
@@ -61,5 +61,6 @@ export async function getStaticProps() {
 			data: data ?? null,
 			errors: errors ?? null
 		},
+		revalidate: 10,
 	}
 }
--- headless-cms/head/pages/index.tsx
+++ headless-cms/head/pages/index.tsx
@@ -59,5 +59,6 @@ export async function getStaticProps() {
       data: data ?? null,
       errors: errors ?? null
     },
+	 revalidate: 10,
   }
 }
