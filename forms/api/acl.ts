e2f9b1b06e90f25537262ff5ba75d42c2f79dba1
jonasNobile <jonas.nobile@icloud.com>
2022-02-23 11:26:59 +0100

    Forms: update acl

--- forms/api/acl.ts
+++ forms/api/acl.ts
@@ -33,103 +33,73 @@ const aclFactory = (model: Model.Schema): Acl.Schema => ({
 				},
 			},
 		},
-		// public: {
-		// 	variables: {},
-		// 	stages: '*',
-		// 	s3: {
-		// 		'**': {
-		// 			upload: false,
-		// 			read: true,
-		// 		},
-		// 	},
-		// 	entities: {
-		// 		Article: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Article', true),
-		// 		},
-		// 		Button: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Button', true),
-		// 		},
-		// 		ContactMessage: {
-		// 			predicates: {},
-		// 			operations: {
-		// 				read: allField(model, 'ContactMessage', true),
-		// 				create: allField(model, 'ContactMessage', true),
-		// 				update: allField(model, 'ContactMessage', false),
-		// 				delete: false,
-		// 			},
-		// 		},
-		// 		Content: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Content', true),
-		// 		},
-		// 		ContentPart: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentPart', true),
-		// 		},
-		// 		ContentReference: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentReference', true),
-		// 		},
-		// 		ContentBlock: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentBlock', true),
-		// 		},
-		// 		ContentImage: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentImage', true),
-		// 		},
-		// 		ContentButton: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentButton', true),
-		// 		},
-		// 		ContentFeatureItem: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentFeatureItem', true),
-		// 		},
-		// 		ContentTestimonial: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentTestimonial', true),
-		// 		},
-		// 		TestimonialAuthor: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'TestimonialAuthor', true),
-		// 		},
-		// 		ContentBlogPost: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'ContentBlogPost', true),
-		// 		},
-		// 		Image: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Image', true),
-		// 		},
-		// 		Link: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Link', true),
-		// 		},
-		// 		Menu: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Menu', true),
-		// 		},
-		// 		MenuItem: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'MenuItem', true),
-		// 		},
-		// 		Page: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Page', true),
-		// 		},
-		// 		Seo: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Seo', true),
-		// 		},
-		// 		Setting: {
-		// 			predicates: {},
-		// 			operations: readOnly(model, 'Setting', true),
-		// 		}
-		// 	},
-		// },
+		public: {
+			variables: {},
+			stages: '*',
+			s3: {
+				'**': {
+					upload: true,
+					read: true,
+				},
+			},
+			entities: {
+				Content: {
+					predicates: {},
+					operations: readOnly(model, 'Content', true),
+				},
+				ContentPart: {
+					predicates: {},
+					operations: readOnly(model, 'ContentPart', true),
+				},
+				ContentReference: {
+					predicates: {},
+					operations: readOnly(model, 'ContentReference', true),
+				},
+				File: {
+					predicates: {},
+					operations: readOnly(model, 'File', true),
+				},
+				Form: {
+					predicates: {},
+					operations: readOnly(model, 'Form', true),
+				},
+				FormOption: {
+					predicates: {},
+					operations: readOnly(model, 'FormOption', true),
+				},
+				FormInput: {
+					predicates: {},
+					operations: {
+						read: allField(model, 'FormInput', true),
+						create: allField(model, 'FormInput', false),
+						update: allField(model, 'FormInput', true),
+						delete: false,
+					},
+				},
+				Image: {
+					predicates: {},
+					operations: readOnly(model, 'Image', true),
+				},
+				Response: {
+					predicates: {},
+					operations: {
+						read: allField(model, 'Response', true),
+						create: allField(model, 'Response', true),
+						update: allField(model, 'Response', true),
+						delete: false,
+					},
+				},
+				ResponseAnswer: {
+					predicates: {},
+					operations: {
+						read: allField(model, 'ResponseAnswer', true),
+						create: allField(model, 'ResponseAnswer', true),
+						update: allField(model, 'ResponseAnswer', true),
+						delete: false,
+					},
+				},
+			},
+		},
 	},
 })
 
