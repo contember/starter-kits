3061a4a6fc0a4cfaa0c17cd89e4230eb683e92e3
Jonáš Nobile <jonas.nobile@icloud.com>
2023-05-29 10:40:02 +0200

    feat: add try catch to fetch API

--- headless-cms/website/lib/graphql/gqlfetch.ts
+++ headless-cms/website/lib/graphql/gqlfetch.ts
@@ -1,27 +1,54 @@
-export async function serverSideFetch(query: any, variables?: any) {
-	const response = await fetch(process.env.NEXT_PUBLIC_API_URL ?? '', {
-		method: 'POST',
-		mode: 'cors',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
-		},
-		body: JSON.stringify({ query, variables })
-	})
+interface CustomError extends Error {
+	cause?: {
+		code?: string;
+	}
+}
 
-	return response.json()
+function getFetchErrorMessage(error: unknown): string {
+	if ((error as CustomError)?.cause?.code) {
+		switch ((error as CustomError).cause?.code) {
+			case 'ECONNREFUSED':
+				return 'Failed to fetch data from the API. Please check your environment variables and make sure your API is running.'
+			default:
+				return 'An unknown error occurred. Please try again later.'
+		}
+	}
+
+	return 'An unknown error occurred. Please try again later.'
+}
+
+export async function serverSideFetch(query: any, variables?: any) {
+	try {
+		const response = await fetch(process.env.NEXT_PUBLIC_API_URL ?? '', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
+			},
+			body: JSON.stringify({ query, variables })
+		})
+
+		return response.json()
+	} catch (error) {
+		throw new Error(getFetchErrorMessage(error))
+	}
 }
 
 export async function clientSideFetch(query: any, variables?: any) {
-	const response = await fetch(process.env.NEXT_PUBLIC_API_URL ?? '', {
-		method: 'POST',
-		mode: 'cors',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
-		},
-		body: JSON.stringify({ query, variables })
-	})
+	try {
+		const response = await fetch(process.env.NEXT_PUBLIC_API_URL ?? '', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
+			},
+			body: JSON.stringify({ query, variables })
+		})
 
-	return response.json()
+		return response.json()
+	} catch (error) {
+		throw new Error(getFetchErrorMessage(error))
+	}
 }
